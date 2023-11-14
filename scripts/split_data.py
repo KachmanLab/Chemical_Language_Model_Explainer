4eca7c20de48d4958deabbdf5a1845b137a709c7
Stefan <stefano.hoedl@gmail.com>
2023-11-14 12:33:35 +0100

    flatten+refactor /conf, change train.py, !works

--- scripts/split_data.py
+++ scripts/split_data.py
@@ -19,46 +19,46 @@ from omegaconf import DictConfig, OmegaConf
 @hydra.main(version_base="1.3",
             config_path="/workspace/conf",
             config_name="config")
-def train(cfg: DictConfig) -> None:
+def split(cfg: DictConfig) -> None:
     print('CONFIG')
     print(OmegaConf.to_yaml(cfg))
-    print('CONFIG')
 
-    pl.seed_everything(cfg['ds']['data_seed'])
-    root = f"/workspace/data/{cfg['ds']['task']}/{cfg['ds']['split']}"
+    pl.seed_everything(cfg.split.data_seed)
+    root = f"/workspace/data/{cfg.task.task}/{cfg.split.split}"
 
     train_ds = AqSolDataset(
-        cfg['ds']['filepath'], 'train', cfg['ds']['split'],
-        cfg['ds']['split_frac'], cfg['ds']['n_splits'], cfg['ds']['data_seed'],
+        cfg.task.filepath, 'train', cfg.split.split,
+        cfg.split.split_frac, cfg.split.n_splits, cfg.split.data_seed,
         augment=False)
     valid_ds = AqSolDataset(
-        cfg['ds']['filepath'], 'valid', cfg['ds']['split'],
-        cfg['ds']['split_frac'], cfg['ds']['n_splits'], cfg['ds']['data_seed'])
+        cfg.task.filepath, 'valid', cfg.split.split,
+        cfg.split.split_frac, cfg.split.n_splits, cfg.split.data_seed)
     test_ds = AqSolDataset(
-        cfg['ds']['filepath'], 'test', cfg['ds']['split'],
-        cfg['ds']['split_frac'], cfg['ds']['n_splits'], cfg['ds']['data_seed'])
+        cfg.task.filepath, 'test', cfg.split.split,
+        cfg.split.split_frac, cfg.split.n_splits, cfg.split.data_seed)
 
     test = test_ds[:]
     print(len(test), 'test len')
     with open(f"{root}/test.pkl", 'wb') as f:
         pickle.dump(test, f)
 
-    for fold in range(cfg['ds']['n_splits']):
+    for fold in range(cfg.split.n_splits):
         train = train_ds[fold]
         with open(f"{root}/train{fold}.pkl", 'wb') as f:
             pickle.dump(train, f)
             print(len(train), 'train len', fold)
 
-    for fold in range(cfg['ds']['n_splits']):
+    for fold in range(cfg.split.n_splits):
         valid = valid_ds[fold]
         with open(f"{root}/valid{fold}.pkl", 'wb') as f:
             pickle.dump(valid, f)
             print(len(valid), 'valid len', fold)
 
     # write split into csv for reproducibility
-    propname = cfg['ds']['propname']
+    propname = cfg.task.propname
     results = pd.DataFrame(columns=['SMILES', propname, 'Split'])
-    for subset, data in list(zip(['test', 'val', 'train'], [test, valid, train])):
+    for subset, data in list(zip(
+            ['test', 'valid', 'train'], [test, valid, train])):
         # reverse order for consistency with plotting
         # smiles = list(chain(*[fet('smiles') for f in all]))
         # labels = torch.concat([f.get('labels') for f in all]).cpu().numpy()
@@ -73,8 +73,8 @@ def train(cfg: DictConfig) -> None:
     # reset index to correspond to visualization UID
     results = results.reset_index(drop=True)
     results = results.reset_index().rename(columns={'index': 'uid'})
-    results.to_csv(f"{root}/{cfg['ds']['split']}_df.csv")
+    results.to_csv(f"{root}/{cfg.split.split}_df.csv")
 
 
 if __name__ == "__main__":
-    train()
+    split()
--- scripts/train_model.py
+++ scripts/train_model.py
@@ -6,123 +6,124 @@ import wandb
 from src.model import AqueousRegModel, BaselineAqueousModel, ECFPLinear
 from src.dataloader import ECFPDataSplit
 import pickle
-import dvc.api
 import json
-import gc
 import numpy as np
+import hydra
+from omegaconf import OmegaConf, DictConfig
 
-# import hydra
-# from omegaconf import OmegaConf, DictConfig
-# @hydra.main(version_base=None, config_path="cfg", config_name="config")
-# def main(cfg: DictConfig):
-    # print(OmegaConf.to_yaml(conf))
 
-# print('ds', cfg['ml']['model'], cfg['ml']['head'])
-# print('ml', cfg['ds']['task'], cfg['ds']['split'])
+@hydra.main(version_base="1.3",
+            config_path="/workspace/conf",
+            config_name="config")
+def train(cfg: DictConfig) -> None:
+    print('CONFIG')
+    print(OmegaConf.to_yaml(cfg))
 
-cfg = dvc.api.params_show()
-print(cfg)
-pl.seed_everything(cfg['ml']['seed'])
-root = f"/workspace/data/{cfg['ds']['task']}/{cfg['ds']['split']}"
+    pl.seed_everything(cfg.model.seed)
+    root = f"/workspace/data/{cfg.task.task}/{cfg.split.split}"
 
-with open(f"{root}/test.pkl", 'rb') as f:
-    test = pickle.load(f)
-    if cfg['ml']['model'] == 'ecfp':
-        test = ECFPDataSplit(test, nbits=cfg['ml']['nbits'])
-test_loader = DataLoader(test, batch_size=cfg['ml']['n_batch'],
-                         shuffle=False, num_workers=8)
+    with open(f"{root}/test.pkl", 'rb') as f:
+        test = pickle.load(f)
+        if cfg.model.model == 'ecfp':
+            test = ECFPDataSplit(test, nbits=cfg.model.nbits)
+    test_loader = DataLoader(test, batch_size=cfg.model.n_batch,
+                             shuffle=False, num_workers=8)
 
-metrics = {}
-for fold in range(cfg['ds']['n_splits']):
-    # Load pickle
-    with open(f"{root}/train{fold}.pkl", 'rb') as f:
-        train = pickle.load(f)
-    with open(f"{root}/valid{fold}.pkl", 'rb') as f:
-        valid = pickle.load(f)
-    if cfg['ml']['model'] == 'ecfp':
-        train = ECFPDataSplit(train, nbits=cfg['ml']['nbits'])
-        valid = ECFPDataSplit(valid, nbits=cfg['ml']['nbits'])
-    print('len train, val', len(train), len(valid))
-    train_loader = DataLoader(train, batch_size=cfg['ml']['n_batch'],
-                              shuffle=True, num_workers=8)
-    valid_loader = DataLoader(valid, batch_size=cfg['ml']['n_batch'],
-                              shuffle=False, num_workers=8)
+    metrics = {}
+    for fold in range(cfg.split.n_splits):
+        # Load pickle
+        with open(f"{root}/train{fold}.pkl", 'rb') as f:
+            train = pickle.load(f)
+        with open(f"{root}/valid{fold}.pkl", 'rb') as f:
+            valid = pickle.load(f)
+        if cfg.model.model == 'ecfp':
+            train = ECFPDataSplit(train, nbits=cfg.model.nbits)
+            valid = ECFPDataSplit(valid, nbits=cfg.model.nbits)
+        print('len train, val', len(train), len(valid))
+        train_loader = DataLoader(train, batch_size=cfg.model.n_batch,
+                                  shuffle=True, num_workers=8)
+        valid_loader = DataLoader(valid, batch_size=cfg.model.n_batch,
+                                  shuffle=False, num_workers=8)
 
-    # if fold > 0:
-    #     del model
-    #     del trainer
-    #     del wandb_logger
-    #     torch.cuda.empty_cache()
-    #     gc.collect()
+        # if fold > 0:
+        #     del model
+        #     del trainer
+        #     del wandb_logger
+        #     torch.cuda.empty_cache()
+        #     gc.collect()
 
-    # configure model
-    if cfg['ml']['model'] == 'mmb':
-        model = AqueousRegModel(head=cfg['ml']['head'])
-    elif cfg['ml']['finetune'] or cfg['ml']['model'] == 'mmb-ft':
-        # unfreeze to train the whole model instead of just the head
-        # cfg['finetune'] = True
-        model = AqueousRegModel(head=cfg['ml']['head'])
-        model.mmb.unfreeze()
-    elif cfg['ml']['model'] == 'mmb-avg':
-        model = BaselineAqueousModel(head=cfg['ml']['head'])
-    elif cfg['ml']['model'] == 'ecfp':
-        # model = ECFPLinear(head=head)
-        model = ECFPLinear(head=cfg['ml']['head'],
-                           dim=cfg['ml']['nbits'])
-        # split train script into sklearn - vs - torch
+        # configure model
+        if cfg.model.model == 'mmb':
+            model = AqueousRegModel(head=cfg.head.head)
+        elif cfg.model.finetune or cfg.model.model == 'mmb-ft':
+            # unfreeze to train the whole model instead of just the head
+            # cfg['finetune'] = True
+            model = AqueousRegModel(head=cfg.head.head)
+            model.mmb.unfreeze()
+        elif cfg.model.model == 'mmb-avg':
+            model = BaselineAqueousModel(head=cfg.head.head)
+        elif cfg.model.model == 'ecfp':
+            # model = ECFPLinear(head=head)
+            model = ECFPLinear(head=cfg.head.head,
+                               dim=cfg.model.nbits)
+            # split train script into sklearn - vs - torch
 
-    wandb_logger = WandbLogger(
-        project='aqueous-solu' if cfg['ds']['task'] == 'aq' else cfg['ds']['task']
-    )
-    wandb_logger.experiment.config.update(cfg)
+        wandb_logger = WandbLogger(
+            project='aqueous-solu' if cfg.task.task == 'aq' else cfg.task.task
+        )
+        wandb_logger.experiment.config.update(cfg)
 
-    trainer = pl.Trainer(
-        max_epochs=cfg['ml']['n_epochs'],
-        accelerator='gpu',
-        gpus=1,
-        precision=16,
-        logger=wandb_logger,
-        auto_lr_find=True,
-    )
+        trainer = pl.Trainer(
+            max_epochs=cfg.model.n_epochs,
+            accelerator='gpu',
+            gpus=1,
+            precision=16,
+            logger=wandb_logger,
+            auto_lr_find=True,
+        )
 
-    trainer.fit(model, train_loader, valid_loader)
-    metrics[fold] = trainer.validate(model, valid_loader)[0]
+        trainer.fit(model, train_loader, valid_loader)
+        metrics[fold] = trainer.validate(model, valid_loader)[0]
 
-    basepath = f"/workspace/out/{cfg['ds']['task']}/{cfg['ds']['split']}"
-    mdir = f"{cfg['ml']['model']}-{cfg['ml']['head']}"
+        basepath = f"/workspace/out/{cfg.task.task}/{cfg.split.split}"
+        mdir = f"{cfg.model.model}-{cfg.head.head}"
 
-    if cfg['ml']['finetune']:
-        path = f"{basepath}/{mdir}/model/mmb{fold}.pt"
-        trainer.save_checkpoint(path)
+        if cfg.model.finetune:
+            path = f"{basepath}/{mdir}/model/mmb{fold}.pt"
+            trainer.save_checkpoint(path)
+        else:
+            path = f"{basepath}/{mdir}/model/head{fold}.pt"
+            torch.save(model.head.state_dict(), path)
+            # artifact = wandb.Artifact(f"head{fold}_{mdir}", type='model')
+            # artifact.add_file(path)
+            # wandb_logger.experiment.log_artifact(artifact)
+
+    # select best model based on valid score, test of test set, save best ckpt
+    # TODO fix RMSE and change to val_rmse
+    # best_fold = torch.argmax([v['val_mae'] for k, v in metrics.items()])
+
+    print(metrics)
+    best_fold = [v['val_mae'] for k, v in metrics.items()]
+    print(best_fold)
+    if len(best_fold) > 1:
+        best_fold = np.argmax(best_fold)
     else:
-        path = f"{basepath}/{mdir}/model/head{fold}.pt"
-        torch.save(model.head.state_dict(), path)
-        # artifact = wandb.Artifact(f"head{fold}_{mdir}", type='model')
-        # artifact.add_file(path)
-        # wandb_logger.experiment.log_artifact(artifact)
+        best_fold = 0
+    ckpt_path = f"{basepath}/{mdir}/model/head{best_fold}.pt"
 
-# select best model based on valid score, test of test set, save best ckpt
-# TODO fix RMSE and change to val_rmse
-# best_fold = torch.argmax([v['val_mae'] for k, v in metrics.items()])
+    if cfg.model.finetune or cfg.model.model == 'mmb-ft':
+        model = model.load_from_checkpoint(ckpt_path, head=cfg.head.head)
+        trainer.save_checkpoint(f"{basepath}/{mdir}/best.pt")
+    else:
+        model.head.load_state_dict(torch.load(ckpt_path))
+        torch.save(model.head.state_dict(), f"{basepath}/{mdir}/best.pt")
 
-print(metrics)
-best_fold = [v['val_mae'] for k, v in metrics.items()]
-print(best_fold)
-if len(best_fold) > 1:
-    best_fold = np.argmax(best_fold)
-else:
-    best_fold = 0
-ckpt_path = f"{basepath}/{mdir}/model/head{best_fold}.pt"
+    metrics['valid'] = trainer.validate(model, valid_loader)[0]
+    metrics['test'] = trainer.test(model, test_loader)[0]
+    print(metrics)
+    with open(f"{basepath}/{mdir}/metrics.json", 'w') as f:
+        json.dump(metrics, f)
 
-if cfg['ml']['model'] == 'mmb-ft' or cfg['ml']['finetune']:
-    model = model.load_from_checkpoint(ckpt_path, head=cfg['ml']['head'])
-    trainer.save_checkpoint(f"{basepath}/{mdir}/best.pt")
-else:
-    model.head.load_state_dict(torch.load(ckpt_path))
-    torch.save(model.head.state_dict(), f"{basepath}/{mdir}/best.pt")
 
-metrics['valid'] = trainer.validate(model, valid_loader)[0]
-metrics['test'] = trainer.test(model, test_loader)[0]
-print(metrics)
-with open(f"{basepath}/{mdir}/metrics.json", 'w') as f:
-    json.dump(metrics, f)
+if __name__ == "__main__":
+    train()
