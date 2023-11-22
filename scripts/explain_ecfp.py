a35bb8d49721a443ad558514fe282919c54c31fa
Stefan <stefano.hoedl@gmail.com>
2023-11-22 09:13:18 +0100

    add pred+explain scripts after rename

---
+++ scripts/explain_ecfp.py
@@ -0,0 +1,308 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import AllChem
+from rdkit.Chem.Draw import SimilarityMaps
+from rdkit.Chem.Draw import rdMolDraw2D
+import numpy as np
+from rdkit.Chem.Draw import IPythonConsole
+import json
+import os
+import matplotlib.pyplot as plt
+import pytorch_lightning as pl
+import seaborn as sns
+from matplotlib.colors import Normalize
+import torch
+from src.dataloader import ECFPDataSplit
+from src.model import ECFPLinear
+import hydra
+import pickle
+from omegaconf import OmegaConf, DictConfig
+
+@hydra.main(
+    version_base="1.3", config_path="/workspace/conf", config_name="config")
+def explain_ecfp(cfg: DictConfig) -> None:
+
+    cfg = OmegaConf.load('/workspace/params.yaml')
+    print(OmegaConf.to_yaml(cfg))
+
+    pl.seed_everything(cfg.model.seed)
+    root = f"/workspace/data/{cfg.task.task}/{cfg.split.split}"
+
+    with open(f"{root}/test.pkl", 'rb') as f:
+        test = pickle.load(f)
+    test_dataset = ECFPDataSplit(test, nbits=cfg.model.nbits)
+    # test_loader = DataLoader(test, batch_size=cfg.model.n_batch,
+    #                          shuffle=False, num_workers=8)
+
+    basepath = f"/workspace/out/{cfg.task.task}/{cfg.split.split}"
+    mdir = f"{cfg.model.model}-{cfg.head.head}"
+    ckpt_path = f"{basepath}/{mdir}/best.pt"
+
+    if cfg.model.model == 'ecfp':
+        model = ECFPLinear(head=cfg.head.head, dim=cfg.model.nbits)
+        model.head.load_state_dict(torch.load(ckpt_path))
+    else:
+        raise NotImplementedError
+
+    weights = model.head.fc1.weight[0].cpu().detach().numpy()
+    weights = weights[:, None]
+
+    # TODO ADD torch.ABS() for pos/neg attrib
+    print('using trained model weights', ckpt_path)
+    print(weights.shape)
+    assert weights.shape[0] == cfg.model.nbits
+    bias = model.head.fc1.bias[0].cpu().detach().numpy()
+    print('bias', bias)
+    # vec = torch.abs(self.fc1.weight[0]).cpu().detach().numpy()
+    # fids = [ix for ix, val in sorted(
+    #     enumerate(vec),
+    #     key=lambda a: a[1],
+    #     reverse=True
+    # )]
+    # print(fids[:8])
+
+    def sort_dict_by_weight(bits_dict, weight_vector, topk=9):
+        ''' sort (bit_id: (mol, bit, bit info) dict by regression weight of bit_id)
+            args:
+                bits_dict:  dictionary of (bit: (mol, bit, bitinfo))
+                weights:    vector of fingerprint length, such as regression fit
+            returns:
+                bits_dict, sorted descending by bits with highest weight
+        '''
+        bits_dict = {k: bits_dict.get(k) for k in sorted(
+            bits_dict.keys(),
+            key=lambda id: np.abs(weight_vector)[int(id)],
+            reverse=True
+        )}
+        if topk:
+            bits_dict = {k: v for i, (k, v) in enumerate(bits_dict.items()) if i < topk}
+        return bits_dict
+
+    def get_weights(bits, weight_vector):
+        return [f"{weight_vector[int(b)][0]:.3f}\t#{b}" for b in bits]
+
+    def make_morgan_dict(smi, nbits=cfg.model.nbits):
+        mol = Chem.MolFromSmiles(smi)
+        bi = {}
+        fp = AllChem.GetMorganFingerprintAsBitVect(
+                mol, radius=2, bitInfo=bi, nBits=nbits
+        )
+        bits_dict = {str(x): (mol, x, bi) for x in fp.GetOnBits()}
+        return bits_dict
+
+    def draw_morgan_bits(bits_dict, uid):
+        p = Draw.DrawMorganBits(bits_dict.values(),
+                                molsPerRow=4,
+                                legends=get_weights(bits_dict.keys(), weights),
+                                useSVG=False,
+                                )
+        p.save(f"{basepath}/{mdir}/viz/{uid}_morgan_bits.png")
+        return p
+
+    def attribute_morgan(smi, bits_dict, weights_vec, norm=True):
+        ''' attribute morgan weights to molecule:
+            for each on-bit (x), attribute w_x (regression head weight for x)
+            to each atom involved in this on-bit '''
+        mol = Chem.MolFromSmiles(smi)
+        # for atom in mol.GetAtoms():
+        #     print("Atom index:", atom.GetIdx(), ", Atomic symbol:", atom.GetSymbol())
+
+        atom_weights = np.zeros(len(mol.GetAtoms()))
+        atom_positive = np.zeros(len(mol.GetAtoms()))
+        atom_negative = np.zeros(len(mol.GetAtoms()))
+        for _mol, x, bi in bits_dict.values():
+            assert Chem.MolToSmiles(mol) == Chem.MolToSmiles(_mol)
+            w_x = weights_vec[int(x)]
+            for _bid, tuples in bi.items():
+                atomsToUse = set()
+                for (atomId, radius) in tuples:
+                    # bi.value tuples are (atom_id_CENTER, radius)
+                    atomsToUse.update(extend_morgan(mol, atomId, radius))
+                    # print(_bit, "\t", atomId, "\t", atomsToUse)
+
+                if norm:
+                    w_x = w_x / len(atomsToUse)
+                atom_weights[list(atomsToUse)] += w_x
+
+                if w_x > 0:
+                    atom_positive[list(atomsToUse)] += w_x
+                elif w_x < 0:
+                    atom_negative[list(atomsToUse)] += w_x
+
+        return atom_weights, atom_positive, atom_negative
+
+    def extend_morgan(mol, atomId, radius=2):
+        ''' get atoms in radius centered on atomId. code from rdkit. '''
+        bitPath = Chem.FindAtomEnvironmentOfRadiusN(mol, radius, atomId)
+
+        # get the atoms for highlighting
+        atomsToUse = set((atomId, ))
+        for b in bitPath:
+            atomsToUse.add(mol.GetBondWithIdx(b).GetBeginAtomIdx())
+            atomsToUse.add(mol.GetBondWithIdx(b).GetEndAtomIdx())
+        return atomsToUse
+
+    def to_rdkit_cmap(atom_weight):
+        '''helper to map to shape required by RDkit to visualize '''
+        cmap = sns.light_palette("green", reverse=False, as_cmap=True)
+        # atom_weight = Normalize()(atom_weight)
+        atom_weight = norm(atom_weight)
+        return {i: [tuple(cmap(w))] for i, w in enumerate(atom_weight)}
+
+    def plot_weighted_mol(atom_colors, smiles, logS, pred, uid="", suffix=""):
+        atom_colors = atom_colors
+        bond_colors = {}
+        h_rads = {}
+        h_lw_mult = {}
+
+        label = f'Exp logS: {logS:.2f}, predicted: {pred:.2f}\n{smiles}'
+
+        mol = Chem.MolFromSmiles(smiles)
+        mol = Draw.PrepareMolForDrawing(mol)
+        d = Draw.rdMolDraw2D.MolDraw2DCairo(700, 700)
+        d.drawOptions().padding = 0.0
+
+        d.DrawMoleculeWithHighlights(
+            mol, label, atom_colors, bond_colors, h_rads, h_lw_mult, -1
+        )
+        # todo legend
+        d.FinishDrawing()
+
+        fname = f'{basepath}/{mdir}/viz/{uid}_MorganAttrib{suffix}.png'
+        with open(fname, mode='wb') as f:
+            f.write(d.GetDrawingText())
+        return d
+
+    # plot entire test set:
+    smiles = test_dataset.smiles[:100]
+    labels = test_dataset.labels[:100]
+    ecfps = test_dataset.ecfp[:100]
+    vmin, vmax = 0., 0.
+    for uid, (smi, logs, ecfp) in enumerate(zip(smiles, labels, ecfps)):
+        pred = model(ecfp[None, ...]).detach().numpy().item()
+        # pred = model.predict(ecfp[None, ...]).detach().numpy().item()
+        # pred = model.predict(test_dataset[:64]).detach().numpy().item()
+
+        bits_dict = make_morgan_dict(smi, nbits=cfg.model.nbits)
+        bits_dict = sort_dict_by_weight(bits_dict, weights, topk=cfg.xai.topk)
+        _ = draw_morgan_bits(bits_dict, uid=uid)
+
+        morgan_weights, morgan_pos, morgan_neg = attribute_morgan(
+            smi, bits_dict, weights)
+        # min, max = np.min(morgan_weights), np.max(morgan_weights)
+        # vmin, vmax = np.min(min, vmin), np.max(max, vmax)
+
+        # norm = Normalize(vmin=-7.29, vmax=2.04)
+        norm = Normalize()
+        _ = plot_weighted_mol(to_rdkit_cmap(morgan_weights), smi, logs, pred, uid)
+        norm = Normalize()
+        _ = plot_weighted_mol(to_rdkit_cmap(morgan_pos), smi, logs, pred, uid, '_pos')
+        norm = Normalize()
+        _ = plot_weighted_mol(to_rdkit_cmap(morgan_neg), smi, logs, pred, uid, '_neg')
+#print(vmin, vmax)
+
+#https://github.com/rdkit/rdkit/blob/d9d1fe2838053484027ba9f5f74629069c6984dc/rdkit/Chem/Draw/__init__.py#L947
+
+   #  prOint(type(p))
+   #  import io
+   #  from PIL import Image, ImageDraw, ImageFont
+   #
+   #  try:
+   #      font = ImageFont.truetype("arial.ttf", 24)  # Specify your font path and size here
+   #  except IOError:
+   #      font = ImageFont.load_default()
+   #  #im = Image.open(io.BytesIO(p))
+   #
+   #  im = ImageDraw.Draw(p)
+   #  text_position = (0, 0)
+   #  text_color = (255, 255, 255)
+   #  im.text(text_position, "ASRWUADRTUADRURADNW Your Title Here",
+   #          #fill=text_color, font=font)
+   #          )
+   #  
+   #  print(type(im))
+   #  print(type(p))
+   #  p.save(f"/workspace/results/aqueous/ecfp/{uid}_morgan_bits.png")
+   #  # bytes_container = io.BytesIO()
+   #  # im.save(bytes_container, format='PNG')
+   #  # image_in_bytes = bytes_container.getvalue()
+   # 
+   #
+
+#smi = 'c1ccccc1CC1CC1'
+
+#ECFP (Morgan)
+# fpgen = AllChem.GetMorganGenerator(radius=2)
+# ao = AllChem.AdditionalOutput()
+# ao.CollectBitInfoMap()
+
+#list_bits = [(mol, x, bi) for x in fp.GetOnBits()]   # (mol, bit, bit info)
+#legends = [str(x) for x in fp.GetOnBits()]
+#Draw.DrawMorganBits(list_bits, molsPerRow=4,legends=legends)
+
+
+### SVG
+# drawOptions = Draw.rdMolDraw2D.MolDrawOptions()
+#     drawOptions.continuousHighlight = False
+#     drawOptions.includeMetadata = False
+#     drawOptions.prepareMolsBeforeDrawing = False
+#     drawer = rdMolDraw2D.MolDraw2DSVG(300, 300)
+    #
+    # drawer.FinishDrawing()
+    # svg = drawer.GetDrawingText()
+    # #plt.savefig(p, f"/workspace/results/aqueous/ecfp/{uid}_morgan_bits.svg")
+    # #p.savefig(f"/workspace/results/aqueous/ecfp/{uid}_morgan_bits.svg")
+    # #p.save(f"/workspace/results/aqueous/ecfp/{uid}_morgan_bits.svg")
+    # with open(f"/workspace/results/aqueous/ecfp/{uid}_morgan_bits.svg", 'w') as f:
+    #     f.write(svg)
+
+    #drawer = rdMolDraw2D.MolDraw2DSVG(molSize[0], molSize[1])
+    # p = Draw.DrawMorganBits(bits_dict.values(), molsPerRow=4, 
+    #                     legends=get_weights(bits_dict.keys(), weights))
+    #
+    # print(type(p))
+    # p.save('/workspace/results/aqueous/ecfp/morgan_bits.png')
+ 
+
+#p.savefig('/workspace/results/aqueous/ecfp/morgan_bits.png',
+    #bbox_inches='tight')
+                    #drawOptions=drawOptions)
+                    #legends=dict_bits.keys())  
+#p.save("/workspace/results/aqueous/ecfp/morgan_bits.png")
+#with open('/workspace/results/aqueous/ecfp/morgan_bits.svg', 'w') as f:
+    #f.write(drawer.GetDrawingText())
+    #plt.save(f)
+    #f.write(d.GetDrawingText())
+
+# key = list(bits_dict.keys())[0]
+# tpl = bits_dict.get(key)
+# print(tpl)
+# mol, bitId, bitInfo = tpl
+# atomId = bitInfo[bitId]
+# submol = Chem.MolFromSmiles(Chem.MolFragmentToSmiles(mol, atomsToUse=atomId))
+
+#
+# fp = fpgen.GetFingerprint(mol,additionalOutput=ao)
+# bi = ao.GetBitInfoMap()
+#
+# svgs_ecfp = []
+# for bitid in bi:
+#     mfp2_svg = Draw.DrawMorganBit(mol, bitid, bi, useSVG=True)
+#     svgs_ecfp.append(mfp2_svg)
+#     
+#
+# ## RDKIT FP
+# fpgen = AllChem.GetRDKitFPGenerator()
+# ao = AllChem.AdditionalOutput()
+# ao.CollectBitPaths()
+#
+# fp = fpgen.GetFingerprint(mol,additionalOutput=ao)
+# rdkbi = ao.GetBitPaths()
+#
+# svgs_rd = []
+# for bitid in rdkbi:
+#     rdk_svg = Draw.DrawRDKitBit(mol, bitid, rdkbi, useSVG=True)
+#     svgs_rd.append(rdk_svg)
+
+if __name__ == "__main__":
+    explain_ecfp()
---
+++ scripts/predict_model.py
@@ -0,0 +1,153 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+import matplotlib.pyplot as plt
+import pandas as pd
+from itertools import chain
+from src.model import AqueousRegModel, BaselineAqueousModel, ECFPLinear
+from src.dataloader import ECFPDataSplit
+import pickle
+import hydra
+import json
+from omegaconf import OmegaConf, DictConfig
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import seaborn as sns
+
+@hydra.main(
+    version_base="1.3", config_path="/workspace/conf", config_name="config")
+def predict_model(cfg: DictConfig) -> None:
+    # print(OmegaConf.to_yaml(cfg))
+
+    cfg = OmegaConf.load('/workspace/params.yaml')
+    print('EXPLAIN CONFIG from params.yaml')
+    print(OmegaConf.to_yaml(cfg))
+
+    pl.seed_everything(cfg.model.seed)
+    root = f"/workspace/data/{cfg.task.task}/{cfg.split.split}"
+    basepath = f"/workspace/out/{cfg.task.task}/{cfg.split.split}"
+    mdir = f"{cfg.model.model}-{cfg.head.head}"
+    ckpt_path = f"{basepath}/{mdir}/best.pt"
+
+    # determine best val split (model) and validate results
+    with open(f"{basepath}/{mdir}/metrics.json", 'r') as f:
+        metrics = json.load(f)
+
+    # valid_score = metrics.pop('valid')
+    # test_score = metrics.pop('test')
+    best_fold = np.argmin([v['val_mae'] for k, v in metrics.items() if k not in ['valid', 'test']])
+    print('best', best_fold, 'all', metrics)
+
+    with open(f"{root}/valid{best_fold}.pkl", 'rb') as f:
+        valid = pickle.load(f)
+    with open(f"{root}/test.pkl", 'rb') as f:
+        test = pickle.load(f)
+    if cfg.model.model == 'ecfp':
+        valid = ECFPDataSplit(valid, nbits=cfg.model.nbits)
+        test = ECFPDataSplit(test, nbits=cfg.model.nbits)
+    test_loader = DataLoader(test, batch_size=cfg.model.n_batch,
+                             shuffle=False, num_workers=8)
+    valid_loader = DataLoader(valid, batch_size=cfg.model.n_batch,
+                              shuffle=False, num_workers=8)
+
+    head = cfg.head.head
+    if cfg.model.model == 'mmb':
+        model = AqueousRegModel(head=head)
+        model.head.load_state_dict(torch.load(ckpt_path))
+    if cfg.model.finetune or cfg.model.model == 'mmb-ft':
+        model = AqueousRegModel(head=head)
+        # model = model.load_from_checkpoint(ckpt_path, head=head)
+        mmb_path = f"{basepath}/{mdir}/best_mmb.pt"
+        model.mmb.load_state_dict(torch.load(mmb_path))
+        model.head.load_state_dict(torch.load(ckpt_path))
+    elif cfg.model.model == 'mmb-avg':
+        model = BaselineAqueousModel(head=head)
+    elif cfg.model.model == 'ecfp':
+        model = ECFPLinear(head=cfg.head.head, dim=cfg.model.nbits)
+        model.head.load_state_dict(torch.load(ckpt_path))
+
+    trainer = pl.Trainer(
+        accelerator='gpu',
+        gpus=1,
+        precision=16,
+    )
+
+    metrics[f'val_{best_fold}'] = trainer.validate(model, valid_loader)[0]
+    metrics[f'test_{best_fold}'] = trainer.validate(model, test_loader)[0]
+    print('val scores', metrics[f'val_{best_fold}'], metrics['valid'])
+    print('test scores', metrics[f'test_{best_fold}'], metrics['test'])
+
+    all_valid = trainer.predict(model, valid_loader)
+    all_test = trainer.predict(model, test_loader)
+
+    results = pd.DataFrame(columns=[
+        'SMILES', 'Tokens', 'Prediction', 'Label', 'Split']
+    )
+    for split, all in list(zip(['test', 'valid'], [all_test, all_valid])):
+        # reverse order for consistency with plotting
+        print([f.get('smiles') for f in all])
+        if cfg.model.model == 'ecfp':
+            smiles = test.smiles if split == 'test' else valid.smiles
+            tokens = None
+        else:
+            smiles = list(chain(*[f.get('smiles') for f in all]))
+            tokens = list(chain(*[f.get('tokens') for f in all]))
+        preds = torch.concat([f.get('preds') for f in all]).cpu().numpy()
+        labels = torch.concat([f.get('labels') for f in all]).cpu().numpy()
+
+        res = pd.DataFrame({
+            'SMILES': smiles,
+            'Tokens': tokens,
+            'Prediction': preds,
+            'Label': labels,
+            'Split': split
+            })
+        results = pd.concat([results, res], axis=0)
+
+    # reset index to correspond to visualization UID
+    results = results.reset_index(drop=True)
+    results = results.reset_index().rename(columns={'index': 'uid'})
+    results.to_csv(f"{basepath}/{mdir}/predictions.csv", index=False)
+
+    ###################################
+    yhat = torch.concat([f.get('preds') for f in all_test])
+    y = torch.concat([f.get('labels') for f in all_test])
+
+    mse = nn.MSELoss()(yhat, y)
+    mae = nn.L1Loss()(yhat, y)
+    rmse = torch.sqrt(mse)
+
+    data = pd.DataFrame({'y': y, 'yhat': yhat})
+    reg = LinearRegression()
+    reg.fit(yhat.reshape(-1, 1), y)
+    slo = f"{reg.coef_[0]:.3f}"
+
+    # text formatting for plot
+    split = f"{int(round(1.-cfg.split.split_frac, 2)*100)}% "
+    color = cfg.split.color
+    _acc = cfg.split.split
+
+    # plot a hexagonal parity plot
+    ally = np.array(torch.concat([y, yhat], axis=0))
+    lim = [np.floor(np.min(ally)), np.ceil(np.max(ally))]
+    print('lim', lim)
+    p = sns.jointplot(x=y, y=yhat, kind='hex', color=color,
+                      xlim=lim, ylim=lim)
+    sns.regplot(x="yhat", y="y", data=data, ax=p.ax_joint,
+                color='grey', ci=None, scatter=False)
+    p.fig.suptitle(f"{cfg.task.plot_title} parity plot \
+        \n{_acc} {split}test set")
+    p.set_axis_labels(f"Experimental {cfg.task.plot_propname}",
+                      f"Model {cfg.task.plot_propname}")
+
+    p.fig.subplots_adjust(top=0.95)
+    p.fig.tight_layout()
+    txt = f"RMSE = {rmse:.3f} \nMAE = {mae:.3f} \nn = {len(y)} \nSlope = {slo}"
+    plt.text(lim[1], lim[0], txt, ha="right", va="bottom", fontsize=14)
+    # plt.text(1, 0, txt, ha="right", va="bottom", fontsize=14)
+    p.savefig(f"{basepath}/{mdir}/parity_plot.png")
+
+
+if __name__ == "__main__":
+    predict_model()
