d94428bb70a93ec181c55c68788f2bfda2a57322
Stefan Hoedl <stefano.hoedl@gmail.com>
2024-02-13 14:29:06 +0000

    fix cbar cos sim

--- scripts/plot_similarity.py
+++ scripts/plot_similarity.py
@@ -40,83 +40,34 @@ def plot_similarity():
             print(f"File not found: {basepath}/{mdir}")
             continue
 
-    attributions['all-equal'] = [np.ones_like(attr) / len(attr) for attr in attribs]
+    attributions['all-equal'] = [
+        np.ones_like(attr) / len(attr) for attr in attribs
+    ]
 
-    similarities = []
-    # Calculate pairwise cosine similarity
-    attribszip = list(zip(*attributions.values()))
-    # data = attributions.values()
-    # for ix in range(len(attribs)):
-    for ix in attribszip:
-        print(ix)
-        print([len(i) for i in ix])
-        similarities.append(
-            cosine_similarity(np.array(ix))
-        )
+    similarities = [
+        cosine_similarity(np.array(ix)) for ix in
+        list(zip(*attributions.values()))
+    ]
 
     print(similarities)
     similarity_matrix = np.mean(np.array(similarities), axis=0)
     mask = np.triu(np.ones_like(similarity_matrix, dtype=bool), k=1)
 
-    # Plotting the heatmap
     plt.figure(figsize=(10, 8))
     sns.heatmap(similarity_matrix,
                 annot=True,
                 mask=mask,
                 xticklabels=attributions.keys(),
                 yticklabels=attributions.keys(),
+                cbar_kws={'label': 'Average cosine similarity'},
                 )
 
-    plt.title("Pairwise cosine similarity of attributed relevance")
+    plt.title(f"Pairwise cosine similarity of attributed relevance\n\
+              averaged over {cfg.split.split} test set")
+
     plt.tight_layout()
     plt.savefig(f"{basepath}/attribution_comparison.png")
-
-    # models = metrics.keys()
-    #
-    # # Updated plotting with the revised color scheme
-    # fig, ax = plt.subplots(figsize=(14, 8))
-    # width = 0.35  # Width of the bars
-    #
-    # colors_dark = plt.cm.viridis(np.linspace(0, 0.75, len(models)))
-    # colors_light = plt.cm.viridis(np.linspace(0.05, 0.8, len(models)))
-    #
-    # # colors_light = colors_dark + np.array([0.3, 0.3, 0.3, 0])
-    # # colors_light = np.clip(colors_light, 0, 1)  # Ensuring valid color values
-    #
-    # # base_colors = plt.cm.tab10(np.linspace(0, 1, len(models)))
-    # # colors_light = base_colors + np.array([0.3, 0.3, 0.3, 0])
-    # # colors_light = np.clip(colors_light, 0, 1)  # Ensuring valid color values
-    # # colors_dark = base_colors
-    #
-    # for i, model in enumerate(models):
-    #     # Placeholder data for CV MAE and Test MAE
-    #
-    #     cv_mae_mean = np.mean(metrics[model]['val_mae'])
-    #     cv_mae_std = np.std(metrics[model]['val_mae'])
-    #     test_mae = metrics[model]['test_mae']
-    #
-    #     # Plotting CV MAE (lighter color)
-    #     ax.bar(i - width/2, cv_mae_mean, width, yerr=cv_mae_std,
-    #            color=colors_light[i], label=f'{model} (Valid)')
-    #
-    #     # Plotting Test MAE (darker color)
-    #     ax.bar(i + width/2, test_mae, width, color=colors_dark[i],
-    #            alpha=0.7, label=f'{model} (Test)')
-    #
-    # # Adding labels and title
-    # ax.set_ylabel('MAE')
-    # ax.set_title(f'Model comparison: {cfg.task.plot_title}, {cfg.split.split} \
-    #              Split, Mean Absolute Error (MAE))')
-    # ax.set_xticks(range(len(models)))
-    # ax.set_xticklabels(models, rotation=45, ha='right')
-    # ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
-    #
-    # plt.tight_layout()
-    # plt.savefig(f"{basepath}/model_comparision_mae.png")
 
 
 if __name__ == "__main__":
     plot_similarity()
-    # plot_models(task='aq', split='random')
-    # plot_models(task='aq', split='accurate')
-    # plot_models(task='aq', split='scaffold')
